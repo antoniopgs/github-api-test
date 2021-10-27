dd4502cb79a09b554f0e3faf4e8a78d965916f1f
António Gonçalves <antoniogoncalves@Antonios-MacBook-Pro.local>
2021-10-27 12:27:51 +0100

    more stuff

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@
 const owner = "antoniopgs";
 const repo = "github-api-test";
 
-const proposalNumber = 9;
-const proposalTitle = "Fund I";
-const proposalDescription = "Deploy Fund I at address 0x9";
+const proposalNumber = 8;
+const proposalTitle = "Fund H";
+const proposalDescription = "Deploy Fund H at address 0x8";
 
 const discourseEoiUrl = "https://www.example1.com";
 const discourseProposalUrl = "https://www.example1.com";
@@ -13,17 +13,22 @@ const snapshotVoteUrl = "https://www.example1.com";
 const details = "Issue Bla Bla"; // issue details
 const discussion = "Implementation Bla Bla"; // implementation details
 
-const implementArgs = {
-    foo: "This", 
-    bar: "works!"
+const implementArgs = { // edit the key value pairs
+    foo: "Hello", 
+    bar: "world"
 };
 
-function implement(implementArgs) { // must return two arrays (1st for targets, 2nd for data)
-    console.log("Lorem Ipsum");
+function implement(implementArgs) { // must return object with targets array and data array
+    console.log(`${foo}, ${bar}!`);
+
+    return {
+        targets: [1, 2, 3],
+        data: ["a", "b", "c"]
+    }
 }
 
-const json = {
-    "name": "Code 423n4 (Arena) Audit - Bug Bounty #2",
+const json = { // edit the calls array
+    "name": `${proposalTitle}`,
     "calls": [
         {
             "target": "USDC",
@@ -43,6 +48,12 @@ const json = {
         }
     ] 
 };
+
+/*
+if (implement(implementArgs) !== daoCheckTool) {
+    throw new Error("implementation does not match DAOCheckTool json output");
+}
+*/
 
 // ----- DO NOT CHANGE -----
 const createAll = require("./utils/create");
