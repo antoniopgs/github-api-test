cf8d73fcbd1e0e37399d5762b351ae00262d7b27
António Gonçalves <antoniogoncalves@Antonios-MacBook-Pro.local>
2021-10-27 08:52:25 +0100

    update stuff

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const Web3 = require("web3");
 const axios = require("axios");
+require("dotenv").config();
 
-const API_TOKEN = "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";
+const web3 = new Web3();
 
 const createIssue = async (owner, repo, title, body) => {
     const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
@@ -15,7 +17,7 @@ const createIssue = async (owner, repo, title, body) => {
 
     const response = await axios.post(url, json, {
         headers: {
-            Authorization: `token ${API_TOKEN}`
+            Authorization: `token ${process.env.API_TEST_TOKEN}`
         }
     });
 
@@ -25,6 +27,40 @@ const createIssue = async (owner, repo, title, body) => {
     return response.data.html_url;
 };
 
+const createBranch = async (ref) => {
+    try {
+        const owner = "antoniopgs";
+        const repo = "api-test";
+        const sha = web3.utils.sha3(ref);
+        const slicedSha = sha.slice(2, 42);
+
+        const url = `https://api.github.com/repos/${owner}/${repo}/git/refs`;
+
+        const json = {
+            accept: "application/vnd.github.v3+json",
+            owner: owner,
+            repo: repo,
+            ref: `refs/heads/${ref}`,
+            sha: slicedSha,
+        };
+
+        const response = await axios.post(url, json, {
+            headers: {
+                Authorization: `token ${process.env.API_TEST_TOKEN}`
+            }
+        });
+
+        console.log(response.status, response.statusText);
+
+    } catch (err) {
+        console.error(`Status Code: ${err.response.status} | Status Msg: ${err.response.statusText} | Msg: ${err.response.data.message}`);
+    }
+}
+
+const createFile = () => {
+
+}
+
 const createPR = async (owner, repo, title, head, base, body, maintainer_can_modify, draft, issue) => {
     try {
         const url = `https://api.github.com/repos/${owner}/${repo}/pulls`;
@@ -33,22 +69,22 @@ const createPR = async (owner, repo, title, head, base, body, maintainer_can_mod
             accept: "application/vnd.github.v3+json",
             owner: owner,
             repo: repo,
-            title: title,
+            //title: title,
             head: head,
             base: base,
             body: body,
             maintainer_can_modify: maintainer_can_modify,
             draft: draft,
-            //issue: issue
+            issue: issue
         }
 
         const response = await axios.post(url, json, {
             headers: {
-                Authorization: `token ${API_TOKEN}`
+                Authorization: `token ${process.env.API_TEST_TOKEN}`
             }
         });
         console.log(response.status, response.statusText);
-        console.log("Data:", response.data);
+        //console.log("Data:", response.data);
 
     } catch (err) {
         console.error(`Status Code: ${err.response.status} | Status Msg: ${err.response.statusText} | Msg: ${err.response.data.message}`);
@@ -62,4 +98,5 @@ const createAll = async () => {
 }
 
 //createIssue("antoniopgs", "api-test", "New Issue XXX", "Lorem Ipsum 123");
-createPR("antoniopgs", "api-test", "new", "branch-1", "master", "Bla", false, false, 5);
+//createPR("antoniopgs", "api-test", "new 2", "branch-1", "master", "Bla", false, false, 5);
+createBranch("branch-2");
