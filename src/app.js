16546f703a8dde1ebea733abee0e9f24edea33c0
António Gonçalves <antoniogoncalves@Antonios-MacBook-Pro.local>
2021-11-09 08:09:03 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,22 +1,29 @@
 // ----- EDIT VAR VALUES TO FIT YOUR NEEDS -----
-const repoOwner = "antoniopgs";
-const repoName = "github-api-test";
-
-const proposalNumber = 818;
-const proposalTitle = "Fund byb";
-const proposalDescription = "Deploy Fund byb at address 0x000";
+const proposalNumber = 1936;
+const proposalTitle = "Growth WW2";
+const proposalDescription = `\
+Deploy Fund byb at address 0x000
+- Address A: \`0xE94A8d2eda89866C628AfF5Ff1461DFD22fC5DcD\`
+- Amount B: \`50_000\``;
 
 const discourseEoiUrl = "https://www.example1.com";
 const discourseProposalUrl = "https://www.example1.com";
 const snapshotVoteUrl = "https://www.example1.com";
 
-const issueDetails = "Issue Bla Bla";
-const implementationDiscussion = "Implementation Bla Bla";
+const issueDetails = `\
+- Address A: \`0xE94A8d2eda89866C628AfF5Ff1461DFD22fC5DcD\`
+- Amount B: \`50_000\``;
 
-// must return object with targets array and data array
+const implementationDiscussion = `\
+- Address A: \`0xE94A8d2eda89866C628AfF5Ff1461DFD22fC5DcD\`
+- Amount B: \`50_000\``;
+
 function implement() {
+
+    // ----- EDIT HERE -----
     const targets = [1, 2, 3];
     const data = ["a", "b", "c"];
+    // ----- STOP EDITING -----
 
     return {
         targets: targets,
@@ -50,8 +57,6 @@ const json = {
 // ----- DO NOT CHANGE -----
 const run = require("./utils/create");
 run(
-    repoOwner,
-    repoName,
     proposalNumber,
     proposalTitle,
     proposalDescription,
--- src/utils/create.js
+++ src/utils/create.js
@@ -5,15 +5,15 @@ const getImplementationContent = require("./implementationContent");
 
 const baseUrl = "https://api.github.com";
 
-const createIssue = async (owner, repo, title, body) => {
+const createIssue = async (title, body) => {
     console.log("Creating Issue...");
 
-    const url = `${baseUrl}/repos/${owner}/${repo}/issues`;
+    const url = `${baseUrl}/repos/${process.env.REPO_OWNER}/${process.env.REPO_NAME}/issues`;
 
     const json = {
         accept: "application/vnd.github.v3+json",
-        owner: owner,
-        repo: repo,
+        owner: process.env.REPO_OWNER,
+        repo: process.env.REPO_NAME,
         title: title,
         body: body,
     }
@@ -32,16 +32,14 @@ const createIssue = async (owner, repo, title, body) => {
 const getMasterSha = async () => {
     console.log("Getting Master Sha...");
 
-    const owner = "antoniopgs";
-    const repo = "api-test";
     const ref = "heads/master";
 
-    const url = `${baseUrl}/repos/${owner}/${repo}/git/ref/${ref}`;
+    const url = `${baseUrl}/repos/${process.env.REPO_OWNER}/${process.env.REPO_NAME}/git/ref/${ref}`;
 
     const json = {
         accept: "application/vnd.github.v3+json",
-        owner: owner,
-        repo: repo,
+        owner: process.env.REPO_OWNER,
+        repo: process.env.REPO_NAME,
         ref: ref
     }
 
@@ -56,15 +54,15 @@ const getMasterSha = async () => {
     return response.data.object.sha;
 }
 
-const createBranch = async (owner, repo, branchName) => {
+const createBranch = async branchName => {
     console.log("Creating Branch...");
 
-    const url = `${baseUrl}/repos/${owner}/${repo}/git/refs`;
+    const url = `${baseUrl}/repos/${process.env.REPO_OWNER}/${process.env.REPO_NAME}/git/refs`;
 
     const json = {
         accept: "application/vnd.github.v3+json",
-        owner: owner,
-        repo: repo,
+        owner: process.env.REPO_OWNER,
+        repo: process.env.REPO_NAME,
         ref: `refs/heads/${branchName}`,
         sha: await getMasterSha(),
     };
@@ -78,17 +76,17 @@ const createBranch = async (owner, repo, branchName) => {
     console.log(`Status Code: ${response.status} | Status Msg: ${response.statusText}\n`);
 }
 
-const createImplementation = async (owner, repo, branchName, implementationName, implementationContent) => {
+const createImplementation = async (branchName, implementationName, implementationContent) => {
     console.log("Creating File...");
 
     const commitMessage = `Add ${implementationName}`;
 
-    const url = `${baseUrl}/repos/${owner}/${repo}/contents/${implementationName}`;
+    const url = `${baseUrl}/repos/${process.env.REPO_OWNER}/${process.env.REPO_NAME}/contents/${implementationName}`;
 
     const json = {
         accept: "application/vnd.github.v3+json",
-        owner: owner,
-        repo: repo,
+        owner: process.env.REPO_OWNER,
+        repo: process.env.REPO_NAME,
         path: implementationName,
         message: commitMessage,
         content: Buffer.from(implementationContent).toString('base64'),
@@ -104,17 +102,17 @@ const createImplementation = async (owner, repo, branchName, implementationName,
     console.log(`Status Code: ${response.status} | Status Msg: ${response.statusText}\n`);
 }
 
-const createPR = async (owner, repo, issueTitle, issueUrl, head, maintainer_can_modify, draft) => {
+const createPR = async (issueTitle, issueUrl, head, maintainer_can_modify, draft) => {
     console.log("Creating Pull Request...");
 
-    const url = `${baseUrl}/repos/${owner}/${repo}/pulls`;
+    const url = `${baseUrl}/repos/${process.env.REPO_OWNER}/${process.env.REPO_NAME}/pulls`;
     const title = `Add ${issueTitle}`;
     const body = `# Proposal Number - Proposal Title\nIssue: ${issueUrl}`;
 
     const json = {
         accept: "application/vnd.github.v3+json",
-        owner: owner,
-        repo: repo,
+        owner: process.env.REPO_OWNER,
+        repo: process.env.REPO_NAME,
         title: title,
         head: head,
         base: "master",
@@ -132,23 +130,23 @@ const createPR = async (owner, repo, issueTitle, issueUrl, head, maintainer_can_
     console.log(`Status Code: ${response.status} | Status Msg: ${response.statusText}\n`);
 }
 
-const createAll = async (owner, repo, proposalNumber, proposalTitle, proposalDescription, discourseEoiUrl, discourseProposalUrl, snapshotVoteUrl, details, discussion, code, json) => {
+const createAll = async (proposalNumber, proposalTitle, proposalDescription, discourseEoiUrl, discourseProposalUrl, snapshotVoteUrl, details, discussion, code, json) => {
     try {
         const maintainerCanModify = false;
         const draft = false;
 
         const issueTitle = `Proposal #${proposalNumber} - ${proposalTitle}`;
         const issueBody = getIssueBody(proposalNumber, proposalTitle, proposalDescription, discourseEoiUrl, discourseProposalUrl, snapshotVoteUrl, details);
-        const issueUrl = await createIssue(owner, repo, issueTitle, issueBody);
+        const issueUrl = await createIssue(issueTitle, issueBody);
 
         const branchName = proposalTitle.toLowerCase().replace(/ /g, "-");
-        await createBranch(owner, repo, branchName);
+        await createBranch(branchName);
 
         const implementationName = `${proposalNumber}-${branchName}.md`;
         const implementationContent = getImplementationContent(proposalNumber, proposalTitle, proposalDescription, issueUrl, discussion, code, json);
-        await createImplementation(owner, repo, branchName, implementationName, implementationContent);
+        await createImplementation(branchName, implementationName, implementationContent);
 
-        await createPR(owner, repo, issueTitle, issueUrl, branchName, maintainerCanModify, draft);
+        await createPR(issueTitle, issueUrl, branchName, maintainerCanModify, draft);
 
         console.log("All done!");
 
@@ -157,7 +155,7 @@ const createAll = async (owner, repo, proposalNumber, proposalTitle, proposalDes
     }
 }
 
-const run = async (owner, repo, proposalNumber, proposalTitle, proposalDescription, discourseEoiUrl, discourseProposalUrl, snapshotVoteUrl, details, discussion, code, json) => {
+const run = async (proposalNumber, proposalTitle, proposalDescription, discourseEoiUrl, discourseProposalUrl, snapshotVoteUrl, details, discussion, code, json) => {
     try {
         /*
         if (implement() !== daoCheckTool(json)) {
@@ -165,7 +163,7 @@ const run = async (owner, repo, proposalNumber, proposalTitle, proposalDescripti
         }
         */
 
-        await createAll(owner, repo, proposalNumber, proposalTitle, proposalDescription, discourseEoiUrl, discourseProposalUrl, snapshotVoteUrl, details, discussion, code, json);
+        await createAll(proposalNumber, proposalTitle, proposalDescription, discourseEoiUrl, discourseProposalUrl, snapshotVoteUrl, details, discussion, code, json);
 
     } catch (err) {
         console.error(err.message);
